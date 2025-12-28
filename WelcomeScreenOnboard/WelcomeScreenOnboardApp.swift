f28acada50bb7329446b177d016f7170b164ebfd
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-28 09:56:14 +0700

    Initial Commit

---
+++ WelcomeScreenOnboard/WelcomeScreenOnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenOnboardApp.swift
+//  WelcomeScreenOnboard
+//
+//  Created by Iqbal Alhadad on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenOnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
